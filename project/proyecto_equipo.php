75fb08fc2f1bba2fe8dca9d597d7c66547279fd0
sagrasistema <120744252+sagrasistema@users.noreply.github.com>
2026-07-27 08:12:56 -0400

    asd

--- project/proyecto_equipo.php
+++ project/proyecto_equipo.php
@@ -5,7 +5,7 @@
  */
 
 
-require_once 'main/config';
+require_once '../main/config';
 
 if (!isset($pdo) || !$pdo instanceof PDO) {
     error_log('Error crítico: La variable $pdo no está inicializada.');
