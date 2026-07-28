899d55c341a728985bc97f864a77ca996e8fd214
sagrasistema <120744252+sagrasistema@users.noreply.github.com>
2026-07-28 08:07:14 -0400

    ad

--- project/actividades.php
+++ project/actividades.php
@@ -154,7 +154,7 @@ include 'conect-actividades.php';
             if ((int)$pruebaId === 11) {
                 include 'prueba11.php';
             } elseif ((int)$pruebaId === 16) {
-                include 'prueba16.php';
+                include 'prueba23.php';
             } elseif  ((int)$pruebaId === 23) {
                 include 'prueba23.php';
             }
