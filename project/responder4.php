c8ec0a0a67fb64df3b026722cfb0128162b19430
sagrasistema <120744252+sagrasistema@users.noreply.github.com>
2026-07-29 09:12:13 -0400

    s

--- project/responder4.php
+++ project/responder4.php
@@ -29,13 +29,13 @@ $proyectoId = filter_input(INPUT_GET, 'proyectoId', FILTER_VALIDATE_INT) ?? 0;
     <a href="responder.php?proyectoId=<?php echo $proyectoId; ?>" class="stage-btn ">
         <i class="ri-calendar-check-line"></i> Planificación
     </a>
-    <a href="responder2.php?proyectoId=<?php echo $proyectoId; ?>" class="stage-btn active">
+    <a href="responder2.php?proyectoId=<?php echo $proyectoId; ?>" class="stage-btn ">
         <i class="ri-compass-3-line"></i> Estrategia
     </a>
     <a href="responder2.php?proyectoId=<?php echo $proyectoId; ?>" class="stage-btn">
         <i class="ri-play-circle-line"></i> Ejecución
     </a>
-    <a href="responder4.php?proyectoId=<?php echo $proyectoId; ?>" class="stage-btn">
+    <a href="responder4.php?proyectoId=<?php echo $proyectoId; ?>" class="stage-btn active">
         <i class="ri-flag-line"></i> Conclusión
     </a>
 </div>
