f60ec46f38895a15a9d406e242510526c6eb2561
sagrasistema <120744252+sagrasistema@users.noreply.github.com>
2026-08-03 09:02:31 -0400

    d

--- login.php
+++ login.php
@@ -68,6 +68,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar Sesión - SAGRAGP</title>
+    <link rel="shortcut icon" href="client/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="client/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     
     <style>
