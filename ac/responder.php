d47cf2c45aedfd7bdc99284770f826d88b6af9f4
sagrasistema <120744252+sagrasistema@users.noreply.github.com>
2026-08-05 09:46:58 -0400

    e

--- ac/responder.php
+++ ac/responder.php
@@ -318,11 +318,11 @@ $angle = -90 + (($clampedScore - 0) / (105 - 0)) * 180;
                     <thead>
                         <tr style="background: background: #1e293b">
                             <th style="width: 12%; border: 1px solid #e2e8f0; padding: 0.6rem;  color: #ffffff;background: #1e293b">ID Riesgo</th>
-                            <th style="width: 20%; border: 1px solid #e2e8f0; padding: 0.6rem; color: #ffffff";background: #1e293b>Categoría de Riesgo</th>
-                            <th style="width: 32%; border: 1px solid #e2e8f0; padding: 0.6rem; color: #ffffff";background: #1e293b>Descripción del Riesgo Identificado</th>
-                            <th style="width: 22%; border: 1px solid #e2e8f0; padding: 0.6rem; color: #ffffff";background: #1e293b>Factor / Causa Raíz (Según AC)</th>
-                            <th style="width: 14%; border: 1px solid #e2e8f0; padding: 0.6rem; text-align: center; color: #ffffff";background: #1e293b>Nivel de Riesgo Inherente</th>
-                            <th style="width: 5%; border: 1px solid #e2e8f0; padding: 0.6rem; text-align: center; color: #ffffff";background: #1e293b>Acción</th>
+                            <th style="width: 20%; border: 1px solid #e2e8f0; padding: 0.6rem; color: #ffffff; background: #1e293b">Categoría de Riesgo</th>
+                            <th style="width: 32%; border: 1px solid #e2e8f0; padding: 0.6rem; color: #ffffff; background: #1e293b">Descripción del Riesgo Identificado</th>
+                            <th style="width: 22%; border: 1px solid #e2e8f0; padding: 0.6rem; color: #ffffff; background: #1e293b">Factor / Causa Raíz (Según AC)</th>
+                            <th style="width: 14%; border: 1px solid #e2e8f0; padding: 0.6rem; text-align: center; color: #ffffff; background: #1e293b">Nivel de Riesgo Inherente</th>
+                            <th style="width: 5%; border: 1px solid #e2e8f0; padding: 0.6rem; text-align: center; color: #ffffff; background: #1e293b">Acción</th>
                         </tr>
                     </thead>
                     <tbody id="tbodyMatrizAC">
