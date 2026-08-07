dcf7a76606b1d7f642041ed772c07b578c2f7346
sagrasistema <120744252+sagrasistema@users.noreply.github.com>
2026-08-07 12:00:34 -0400

    ss

--- client/clientes.js
+++ client/clientes.js
@@ -130,7 +130,6 @@ function renderTable() {
             <td>
                 <div class="actions-cell">
                     <a href="editar.php?id=${c.id}" class="btn-icon btn-icon-edit"><i class="ri-edit-line"></i></a>
-                    
                 </div>
             </td>
         `;
