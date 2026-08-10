6170334655d667761adaf5220b391ba913f1ae05
sagrasistema <120744252+sagrasistema@users.noreply.github.com>
2026-08-10 09:58:18 -0400

    asd

--- test/project/actividades.php
+++ test/project/actividades.php
@@ -1100,7 +1100,7 @@ $pruebaId = (int)($metaPrueba->id ?? $pruebaId ?? 0);
                 <!-- Asegúrate de pasar el ID de la tabla audit_pruebas -->
                 <button type="button" 
                         id="btnGuardarFeedbackNorma" 
-                        onclick="procesarGuardadoFeedbackNorma(<?= (int)$metaPrueba->id ?>)" 
+                        onclick="procesarGuardadoFeedbackNorma(<?= (int)$pruebaId ?>)" 
                         class="btn btn-primary">
                     Guardar
                 </button>
