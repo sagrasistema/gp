2927035bab9cf14b919c3ce5129c2f4d9fff4a28
sagrasistema <120744252+sagrasistema@users.noreply.github.com>
2026-08-13 12:18:52 -0400

    w

--- test/project/actividades.php
+++ test/project/actividades.php
@@ -203,7 +203,7 @@ if ($frecuenciaNum <= 0) {
     // -------------------------------------------------------------------------
     try {
         $stmtList = $pdo->prepare("
-            SELECT p.id, p.nombre, p.orden, c.nombre as categoria_nombre 
+            SELECT p.id, p.nombre, p.orden,  p.texto_inadecuado, p.texto_inadecuado2, c.nombre as categoria_nombre 
             FROM audit_pruebas p
             INNER JOIN audit_categorias c ON p.categoria_id = c.id
             INNER JOIN proyecto_pruebas_ejecucion pe ON pe.prueba_id = p.id
