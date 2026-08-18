76418d39e9272667a38073b1b49da17124199017
sagrasistema <120744252+sagrasistema@users.noreply.github.com>
2026-08-18 12:51:37 -0400

    e

--- test/project/index.php
+++ test/project/index.php
@@ -46,6 +46,15 @@ include '../main/layout_header.php';
         <a href="#" class="btn-control-disabled" data-tooltip="Instrucciones" onclick="return false;">
             <i class="ri-book-open-line"></i> 
         </a>
+        <?php if ($permisosModulo1['puede_crear'] == 1) {?>
+            <a href="nuevo.php" class="btn btn-primary" data-tooltip="Crear Registro">
+                <i class="ri-add-line"></i>
+            </a>
+        <?php } else {?>
+            <a href="#" class="btn-control-disabled" data-tooltip="Crear Registro">
+                <i class="ri-add-line"></i>
+            </a>
+        <?php } ?>    
 
         <a href="nuevo.php" class="btn btn-primary" data-tooltip="Crear Registro">
             <i class="ri-add-line"></i>
