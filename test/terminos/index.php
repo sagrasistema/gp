020ca05028674bfdc28fc7ad90f2fb401552a5ef
sagrasistema <120744252+sagrasistema@users.noreply.github.com>
2026-08-18 12:27:26 -0400

    asd

--- test/terminos/index.php
+++ test/terminos/index.php
@@ -144,9 +144,6 @@ include '../main/layout_header.php';
             </a>
         <?php } ?>    
 
-        <button onclick="openModal()" class="btn btn-primary" data-tooltip="Crear Registro">
-            <i class="ri-add-line"></i> 
-        </button>
         
         <a href="../index.php" class="btn btn-primary" data-tooltip="Cancelar (Atrás)">
             <i class="ri-close-circle-line"></i> 
