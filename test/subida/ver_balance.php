1752adf1e1efb591304758ab7f1f374a8d85c741
sagrasistema <120744252+sagrasistema@users.noreply.github.com>
2026-08-19 12:14:58 -0400

    d

--- test/subida/ver_balance.php
+++ test/subida/ver_balance.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../main/h.php';
 require_once __DIR__ . '/../main/config.php';
-require_once __DIR__ . '/obtener_balance.php';
+require_once __DIR__ . '/balance_service.php';
 
 /** @var PDO $pdo */
 
